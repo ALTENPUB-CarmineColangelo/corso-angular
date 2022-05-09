84d56924df035987abacce4b985a6b64a23722dc
carmine_colangelo <carmine.colangelo2@alten.it>
2022-05-09 19:49:02 +0200

    forkjoin

--- src/app/services/get-pokemon-gender.service.ts
+++ src/app/services/get-pokemon-gender.service.ts
@@ -1,52 +1,65 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject} from "rxjs";
+import {forkJoin} from "rxjs";
 import {pokeApi} from "../utils/utils";
 import {HttpClient} from "@angular/common/http";
 import {BaseResponse} from "../interfaces/BaseResponse";
 import {Genders} from "../interfaces/Genders";
+import {map} from "rxjs/operators";
+
+type TGenders = "females" | "males" | "genderlesses"
+type TGender = Record<TGenders, string[]>
 
 interface PokemonGenderApi {
   id: string
-  name: Genders
+  name: TGenders
   pokemon_species_details: { pokemon_species: BaseResponse, rate: number }[]
 }
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetPokemonGenderService {
 
-  private _females: string[] = []
-  private _males: string[] = []
-  private _genderlesses: string[] = []
+  private _gender: TGender
 
   constructor(private http: HttpClient) { }
 
+  private _getNameList(genderResponse: PokemonGenderApi) {
+    return genderResponse.pokemon_species_details.map(({ pokemon_species: { name} }) => name);
+  }
+
   getPokemonGender(): void {
-    this.http.get(`${pokeApi}/gender/female`).subscribe((res: PokemonGenderApi) => {
-      this._females = res.pokemon_species_details.map(x => x.pokemon_species.name)
-    })
-    this.http.get(`${pokeApi}/gender/male`).subscribe((res: PokemonGenderApi) => {
-      this._males = res.pokemon_species_details.map(x => x.pokemon_species.name)
-    })
-    this.http.get(`${pokeApi}/gender/genderless`).subscribe((res: PokemonGenderApi) => {
-      this._genderlesses = res.pokemon_species_details.map(x => x.pokemon_species.name)
+    forkJoin({
+      females: this.http.get<PokemonGenderApi>(`${pokeApi}/gender/1`),
+      males: this.http.get<PokemonGenderApi>(`${pokeApi}/gender/2`),
+      genderlesses: this.http.get<PokemonGenderApi>(`${pokeApi}/gender/3`)
+    }).pipe(
+      map((res) => {
+        return {
+          females: this._getNameList(res.females),
+          males: this._getNameList(res.males),
+          genderlesses: this._getNameList(res.genderlesses),
+        }
+      })
+    ).subscribe(res => {
+      this._gender = res;
     })
   }
 
   getGender(name: string): Genders[] | undefined {
     let genders: Genders[] | undefined = []
-    if (this._females.includes(name)) {
+    if (this._gender.females.includes(name)) {
       genders.push('female')
     }
-    if (this._males.includes(name)) {
+    if (this._gender.males.includes(name)) {
       genders.push('male')
     }
-    if (this._genderlesses.includes(name)) {
+    if (this._gender.genderlesses.includes(name)) {
       genders.push('genderless')
     }
     if (genders.length === 0) {
-      genders = undefined
+      return [ undefined ]
     }
     return genders
   }
